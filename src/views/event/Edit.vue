071286828b5a408aa97acf13f286d170883c812c
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-29 14:50:14 +0700

    1.6 add edit button in edit page to flashmessage

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,27 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject:['GStore'],
+  methods:{
+    edit(){
+
+      this.GStore.flashMessage = 
+      'You are successfully editted for ' + this.event.title
+      setTimeout(() =>{
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+      this.$router.push({
+        name:'EventDetails',
+        params: {id: this.event.id}
+      })
+    }
+  }
 }
 </script>
